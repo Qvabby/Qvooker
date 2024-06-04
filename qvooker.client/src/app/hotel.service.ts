3e49bfcde067b3f72af7a0f119e623a5b9f427fd
Saba Salukvadze <saluqvadze2006@gmail.com>
2024-06-04 14:39:26 +0400

    fixed hotelservice

--- qvooker.client/src/app/hotel.service.ts
+++ qvooker.client/src/app/hotel.service.ts
@@ -2,14 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AddHotelDto } from './admin/AddHotelDto';
-import { Observable, map } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
-  private apiUrl = 'https://localhost:7071/hotel';
+  private apiUrl = 'https://localhost:7071/Hotel';
   constructor(private http: HttpClient, private router: Router) { }
+
+
+  getHotels(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`).pipe(
+      tap(response => {
+        console.log("response: " + response);
+        return response;
+      })
+    );
+  }
+  getHotel(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`,).pipe(
+      tap(response => {
+        console.log("RESPONSE OF GETTING INDIVIDUAL HOTEL: " + response);
+        return response;
+      })
+    );
+  }
 
 
   addHotel(hotel: AddHotelDto): Observable<any> {
